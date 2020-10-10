04a5bb646f421abc6f596d4af9f1e0919782e369
MamAliMomen <mamalimomen1996@gmail.com>
2020-10-10 14:24:11 +0330

    Add domains sub-layer to base layer at branch branch1

---
+++ src/main/java/com/mamalimomen/base/domains/BaseEntity.java
@@ -0,0 +1,37 @@
+package com.mamalimomen.base.domains;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Transient;
+import java.io.Serializable;
+
+@MappedSuperclass
+public abstract class BaseEntity<PK extends Number> implements Serializable {
+
+    @Transient
+    private static final long serialVersionUID = -4281565028591599756L;
+
+    @Id
+    @Column(name = "id", updatable = false, nullable = false, unique = true)
+    private PK id;
+
+    @Column(name = "is_deleted", nullable = false)
+    private Boolean deleted = false;
+
+    public PK getId() {
+        return id;
+    }
+
+    public void setId(PK id) {
+        this.id = id;
+    }
+
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+}
