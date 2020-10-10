c12ce722dc18ff65c500ea5ae4ee64442a930774
MamAliMomen <mamalimomen1996@gmail.com>
2020-10-10 23:59:49 +0330

    Add repositories tests at branch branch1

---
+++ src/test/java/com/mamalimomen/repositories/impl/AccountRepositoryImplTest.java
@@ -0,0 +1,26 @@
+package com.mamalimomen.repositories.impl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountRepositoryImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findOneActiveAccountByUsername() {
+    }
+
+    @Test
+    void findOneAccountByUsername() {
+    }
+}
