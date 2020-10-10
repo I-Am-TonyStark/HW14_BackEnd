00e5bfc641f6a489ead0e5bfc3259995cfebe8a3
MamAliMomen <mamalimomen1996@gmail.com>
2020-10-11 00:00:14 +0330

    Add dtos layer at branch branch1

---
+++ src/main/java/com/mamalimomen/services/dtos/AccountDTO.java
@@ -0,0 +1,6 @@
+package com.mamalimomen.services.dtos;
+
+import com.mamalimomen.base.services.dtos.BaseDTO;
+
+public class AccountDTO extends BaseDTO<Long> {
+}
---
+++ src/main/java/com/mamalimomen/services/dtos/PostDTO.java
@@ -0,0 +1,6 @@
+package com.mamalimomen.services.dtos;
+
+import com.mamalimomen.base.services.dtos.BaseDTO;
+
+public class PostDTO extends BaseDTO<Long> {
+}
